44781de2e0366407589f103adc2c22fffa5ed28d
natalieduerr <natalieduerr98@gmail.com>
2018-12-09 20:45:32 -0500

    website update

---
+++ home_game.js
@@ -0,0 +1,170 @@
+$(document).ready(function() {
+    
+    ///////////// BACKGROUND DRAW GAME
+    // initial state of website is view
+    // switch to play on click of button
+    var state = "view"; 
+
+    // initial size of the circles, shrinks and grews based on +/- keys
+    var size = 100;
+
+    // initial mouse coordinate values
+    var xVal = 0;
+    var yVal = 0;
+
+    // captures initial width of the screen
+    const screenWidth = $(document).width();
+
+    // captures initial height of the screen
+    const screenHeight = $(document).height();
+
+
+    // start game
+    // - move the home content up and away
+    // - bring up the instructions and view button
+    // - add circle that follows the mouse
+    // - switch the state to play
+    function startGame(){
+        $('.home').css({'margin-top': '-110vh'});
+        instruct();
+        $( "<div class = 'follow-circle' style = 'width: " + size + "px; height: " + size + "px;'></div>" ).appendTo( '.back' );
+        state = "play";
+    }
+    // end game 
+    // - move the home content back on screen
+    // - hide the instructions
+    // - hide the view button
+    // - disappear the circle that follows the mouse
+    // - disappear the instructions
+    // - switch the state to view
+    function endGame() {
+        $('.home').css({'margin-top': '0px'});
+        hide('.instructions');
+        hide('.view-button');
+        disappear('.follow-circle');
+        disappear('.instructions');
+        state = "view";
+    }
+
+    // When play button is clicked...
+    $('.game-click').click(function () {
+        // if the state was view -> start game
+
+        if (state == "view"){
+            startGame();
+        }
+        // if the state was play -> end game
+        else if (state == "play"){
+            endGame();
+        }
+    });
+
+    $('.game-click').keydown(function (e) {
+            //creates local keycode variable
+            var keyCode = e.which;
+
+            if (keyCode == 13){
+                if (state=="view") {
+                    startGame();
+                }
+                else if (state=="play") {
+                    endGame();
+                }
+            }
+
+    });
+
+    function instruct() {
+        // make the circles go away, brings the instructions up,
+        // and changes the button content to be View
+        $('.instructions').css({'opacity': '1', 'display': 'inherit'});
+        $('.view-button').css({'opacity':'1', 'transition': '.4s'})
+    }
+
+    //given an element, makes the opacity 0
+    function hide(element) {
+        $(element).css({'opacity': '0'});
+    }
+
+    // given an element, make the display none
+    function disappear(element){
+        $(element).css({'display':'none'});
+    }
+
+    // adds new circle to the background element
+    function letsDraw(){
+        $( "<div class = 'new-circles'" + whereTo() + "></div>" ).appendTo( '.back' );
+    }
+
+    function placeCircle(xVal, yVal) {
+        $('.follow-circle').css({
+            left:xVal,
+            top:yVal
+        });
+    }
+
+    // updates the xVal and yVal based on mouse coordinates
+    // offset by the size of the circle so it is placed in the middle of the mouse
+    $(document).on("mousemove", function(event) {
+        xVal = event.pageX - size/2;
+        yVal = event.pageY - size/2;
+
+        placeCircle(xVal,yVal);
+
+    });
+
+    // adds styling to the letsDraw circle element to place the circle with xVal and yVal
+    // also adds size 
+    function whereTo(){
+        return "style = 'width: " + size + "px; height: " + size + "px; top:" + yVal + "px; left:" + xVal + "px;'";
+    }
+
+    // when the background is click, start drawing
+    $('.back').click(function () {
+        if (state == "play"){
+            letsDraw();
+            console.log("background clicked");
+        }
+    })
+
+    // when + or - key is pressed during play state, update the size of the circle
+    $(document).keydown(function(e) {
+        //creates local keycode variable
+        var keyCode = e.which;
+
+         if (state == "play") {
+            if (keyCode == 187) { //if + key
+                size+=50;
+            }
+            if (keyCode == 189 && size!=50) { //if - key and size isnt 0
+                size-=50;
+            }
+            if (keyCode == 37 && xVal > 0) { //if left arrow and on screen
+                xVal-=50;
+                placeCircle(xVal,yVal);
+            }
+            if (keyCode ==  38 && yVal > 0) { //if up arrow and on screen
+                yVal-=50;
+                placeCircle(xVal,yVal);
+            }
+            if (keyCode == 39 && xVal < (screenWidth-size) ) { //if right arrow and on screen
+                xVal+=50;
+                placeCircle(xVal,yVal);
+            }
+            if (keyCode == 40 && yVal < (screenHeight-size)) { //if down arrow and on screen
+                yVal+=50;
+                placeCircle(xVal,yVal);
+            }
+            if (keyCode == 80) {
+                letsDraw();
+            }
+        }
+
+        //changes the follow-circle's width and height
+        $('.follow-circle').css({
+            width: size,
+            height: size
+        });
+      });
+        
+});
---
+++ javascript.js
@@ -0,0 +1,91 @@
+$(document).ready(function () {
+
+    // for on scroll animations
+    var $animation_elements = $('.animation-element');
+    var $window = $(window);
+
+    
+    function check_if_in_view() {
+      var windowHeight = $window.height();
+      var windowTopPosition = $window.scrollTop();
+      var windowBottomPosition = (windowTopPosition + windowHeight);
+    
+      $.each($animation_elements, function() {
+        var $element = $(this);
+        var elementHeight = $element.height();
+        var elementTopPosition = $element.offset().top;
+        console.log(elementTopPosition);
+        var elementBottomPosition = (elementTopPosition + elementHeight);
+    
+        //check to see if this current container is within viewport
+        if ((elementBottomPosition >= windowTopPosition) &&
+          (elementTopPosition <= windowBottomPosition)) {
+          $element.addClass('in-view');
+        }
+      });
+    }
+    
+    $window.on('scroll resize', check_if_in_view);
+    $window.trigger('scroll');
+
+    // list of links from the root    
+    var links = $('.list-of-nav').html();
+
+    // adds the list of links to the mobile menu
+    // makes it easier to make changes to the navigation per page
+    $('.hamburger__links').append(links);
+
+    // remove the nav__links from the hamburger links, this allows the hamburger__links to display on mobile
+    $('.hamburger__links ul').removeClass("nav__links");
+
+    // on click or touch of the hamburger area
+    // if the image is currently a hamburger (menu closed)
+    // - move the links down so they are visible on screen
+    // - change the image to the x
+    // if the image is currently an x (menu opened)
+    // - move the links back up so they are not visible on screen
+    // - change the image to the hamburger
+
+    $('.hamburger').on('click touch', function () {
+        if ($('.hamburger img').attr("src") == "hamburger.png") {
+            $('.hamburger__links').css("margin-top", "0vh");
+            $('.hamburger img').attr("src", "x.png");
+            $('.hamburger img').attr('aria-expanded', 'true');
+        }
+        else if ($('.hamburger img').attr("src") == 'x.png') {
+            $('.hamburger__links').css("margin-top", "-100vh");
+            $('.hamburger img').attr("src", "hamburger.png");
+            $('.hamburger img').attr('aria-expanded', 'false');
+        }
+
+    });
+
+
+    // Open Lightbox
+    if($(window).width() >= 1024){
+        $('.accent .row img').on('click', function (e) {
+            e.preventDefault();
+            var image = $(this).attr('src');
+            $('html').addClass('no-scroll');
+            $('body').append('<div class="lightbox-opened"><img src="' + image + '"></div>');
+            if ($(".lightbox-opened img").height() > $(".lightbox-opened").height() ) {
+                $(".lightbox-opened img").css("top", "0");
+                $(".lightbox-opened img").css("margin-top", "5vh");
+            }
+            else {
+                $(".lightbox-opened img").css("top", "auto");
+                $(".lightbox-opened img").css("margin-top", "0vh"); 
+            }
+        });
+    
+        // Close Lightbox
+        $('body').on('click', '.lightbox-opened', function () {
+            $('html').removeClass('no-scroll');
+            $('.lightbox-opened').remove();
+        });
+    
+    }
+   
+});
+
+
