964c78a6c3343416826820c9f535a9a27c730fe2
natalieduerr <natalieduerr98@gmail.com>
2019-01-13 20:42:50 -0500

    fixed accessibility on nav

--- javascript.js
+++ javascript.js
@@ -4,27 +4,26 @@ $(document).ready(function () {
     var $animation_elements = $('.animation-element');
     var $window = $(window);
 
-    
+    //checks if an element on the screen is in view
     function check_if_in_view() {
-      var windowHeight = $window.height();
-      var windowTopPosition = $window.scrollTop();
-      var windowBottomPosition = (windowTopPosition + windowHeight);
-    
-      $.each($animation_elements, function() {
-        var $element = $(this);
-        var elementHeight = $element.height();
-        var elementTopPosition = $element.offset().top;
-        console.log(elementTopPosition);
-        var elementBottomPosition = (elementTopPosition + elementHeight);
-    
-        //check to see if this current container is within viewport
-        if ((elementBottomPosition >= windowTopPosition) &&
-          (elementTopPosition <= windowBottomPosition)) {
-          $element.addClass('in-view');
-        }
-      });
+        var windowHeight = $window.height();
+        var windowTopPosition = $window.scrollTop();
+        var windowBottomPosition = (windowTopPosition + windowHeight);
+
+        $.each($animation_elements, function () {
+            var $element = $(this);
+            var elementHeight = $element.height();
+            var elementTopPosition = $element.offset().top;
+            var elementBottomPosition = (elementTopPosition + elementHeight);
+
+            //check to see if this current container is within viewport
+            if ((elementBottomPosition >= windowTopPosition) &&
+                (elementTopPosition <= windowBottomPosition)) {
+                $element.addClass('in-view');
+            }
+        });
     }
-    
+
     $window.on('scroll resize', check_if_in_view);
     $window.trigger('scroll');
 
@@ -57,35 +56,35 @@ $(document).ready(function () {
             $('.hamburger img').attr("src", "hamburger.png");
             $('.hamburger img').attr('aria-expanded', 'false');
         }
-
     });
 
-
-    // Open Lightbox
-    if($(window).width() >= 1024){
+    // Open Lightbox, only works on small laptop or bigger
+    if ($(window).width() >= 1024) {
         $('.accent .row img').on('click', function (e) {
             e.preventDefault();
             var image = $(this).attr('src');
             $('html').addClass('no-scroll');
             $('body').append('<div class="lightbox-opened"><img src="' + image + '"></div>');
-            if ($(".lightbox-opened img").height() > $(window).height() ) {
+            // if the image height is greater than the window, place image so that the user can scroll to see the whole thing
+            if ($(".lightbox-opened img").height() > $(window).height()) {
                 $(".lightbox-opened img").css("top", "0");
                 $(".lightbox-opened img").css("margin-top", "5%");
             }
+            // else, center the image within the window
             else {
                 $(".lightbox-opened img").css("top", "auto");
-                $(".lightbox-opened img").css("margin-top", "0"); 
+                $(".lightbox-opened img").css("margin-top", "0");
             }
         });
-    
-        // Close Lightbox
+
+        //if the background is clicked, the lightbox is closed
         $('body').on('click', '.lightbox-opened', function () {
             $('html').removeClass('no-scroll');
             $('.lightbox-opened').remove();
         });
-    
+
     }
-   
+
 });
 
 
